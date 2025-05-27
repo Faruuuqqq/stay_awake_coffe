79d160c55acec98a9f5bd537aebc095e8e5e974a
Faruuuqqq <faruqmahdison@gmail.com>
2025-05-27 23:54:39 +0700

    Add error handling middleware and image upload functionality

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-
+const path = require('path');
+const cors = require('cors');
 require("dotenv").config();
 
 const userRoutes = require("./routes/userRoutes");
@@ -13,12 +14,16 @@ const shipmentRoutes = require("./routes/shipmentRoutes");
 const reviewRoutes = require("./routes/reviewRoutes");
 const addressRoutes = require("./routes/addressRoutes");
 
+const errorMiddleware = require("./middlewares/errorMiddleware");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/users", userRoutes);
 app.use("/products", productRoutes);
@@ -29,6 +34,7 @@ app.use("/payment", paymentRoutes);
 app.use("/shipment", shipmentRoutes);
 app.use("/reviews", reviewRoutes);
 app.use("/addresses", addressRoutes);
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
