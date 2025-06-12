7288f7496e8ad25cb77d3129f80f89baa4c6c1cb
Faruuuqqq <faruqmahdison@gmail.com>
2025-06-12 15:14:04 +0700

    delete all file that not needed

--- backend/middlewares/errorMiddleware.js
+++
@@ -1,7 +0,0 @@
-module.exports = (err, req, res, next) => {
-  console.error(err.stack);
-  if(err.name === 'ValidationError') {
-    return res.status(400).json({ error: err.message });
-  }
-  res.status(500).json({ error: 'Internal Server Error' });
-};
